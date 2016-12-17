5c9b770fef616d135d064e4e9f566a615c13c4d4
Mark Stacey <markjstacey@gmail.com>
2016-12-17 16:37:43 -0330

    Rename DatabaseSession to BoltSession

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -11,12 +11,12 @@ const BOLT_PREAMBLE: [u8; 4] = [0x60, 0x60, 0xB0, 0x17];
 const BOLT_SUPPORTED_VERSIONS: [u32; 1] = [ 1 ];
 const BOLT_VERSION_NONE : u32 = 0;
 
-pub struct DatabaseSession {
+pub struct BoltSession {
     stream: TcpStream,
 }
 
-impl DatabaseSession {
-    fn new(mut stream: TcpStream) -> Result<DatabaseSession, io::Error> {
+impl BoltSession {
+    fn new(mut stream: TcpStream) -> Result<BoltSession, io::Error> {
         // send preamble
         stream.write(&BOLT_PREAMBLE);
 
@@ -46,16 +46,16 @@ impl DatabaseSession {
             panic!("No supported versions; Exiting.");
         }
 
-        Ok(DatabaseSession { stream: stream })
+        Ok(BoltSession { stream: stream })
     }
 }
 
-pub fn connect(server: &str, username: &str, password: &str) -> io::Result<DatabaseSession> {
+pub fn connect(server: &str, username: &str, password: &str) -> io::Result<BoltSession> {
     let mut stream = TcpStream::connect(server);
 
     match stream {
         Ok(stream) => Ok(
-            try!(DatabaseSession::new(stream))
+            try!(BoltSession::new(stream))
         ),
         Err(error) => Err(error),
     }
