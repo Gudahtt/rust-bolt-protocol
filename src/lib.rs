4e701ae9d9fc254857123d1930b946010da0e224
Mark Stacey <markjstacey@gmail.com>
2016-12-17 01:37:35 -0330

    Implement handshake

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,54 @@
+extern crate byteorder;
 
 pub mod bolt_client {
     use std::net::TcpStream;
     use std::io;
+    use std::io::Read;
+    use std::io::Write;
+    use std::time::Duration;
+    use byteorder::{BigEndian, ByteOrder, WriteBytesExt};
+
+    const BOLT_PREAMBLE: [u8; 4] = [0x60, 0x60, 0xB0, 0x17];
+    const BOLT_SUPPORTED_VERSIONS: [u32; 1] = [ 1 ];
+    const BOLT_VERSION_NONE : u32 = 0;
 
     pub struct DatabaseSession {
         stream: TcpStream,
+    }
+
+    impl DatabaseSession {
+        fn new(mut stream: TcpStream) -> Result<DatabaseSession, io::Error> {
+            // send preamble
+            stream.write(&BOLT_PREAMBLE);
+
+            // send compatible versions
+            for &version in BOLT_SUPPORTED_VERSIONS.into_iter() {
+                let mut buf = [0x0; 4];
+                BigEndian::write_u32(&mut buf, version);
+                stream.write(&buf);
+            }
+
+            // fill remaining spaces with 'none' version
+            for _ in BOLT_SUPPORTED_VERSIONS.len()..4 {
+                let mut buf = [0x0; 4];
+                BigEndian::write_u32(&mut buf, BOLT_VERSION_NONE);
+                stream.write(&buf);
+            }
+
+            let mut responseBuffer = [0x0; 4];
+
+            stream.set_read_timeout(Some(Duration::new(5, 0)));
+            let response = try!(stream.read_exact(&mut responseBuffer));
+            stream.set_read_timeout(None);
+
+            let version = BigEndian::read_u32(&responseBuffer);
+
+            if version == 0 {
+                panic!("No supported versions; Exiting.");
+            }
+
+            Ok(DatabaseSession { stream: stream })
+        }
     }
 
     pub fn connect(server: &str, username: &str, password: &str) -> io::Result<DatabaseSession> {
@@ -12,7 +56,7 @@ pub mod bolt_client {
 
         match stream {
             Ok(stream) => Ok(
-                DatabaseSession { stream: stream }
+                try!(DatabaseSession::new(stream))
             ),
             Err(error) => Err(error),
         }
